7a9cf21afa3b80b02672d1ee1cfcebc277c7a1c5
Wise Wong <imhwc@icloud.com>
2026-01-21 11:49:37 +0800

    删除敏感配置文件并添加到 .gitignore

--- src/config/output.ts
+++
@@ -1,7 +0,0 @@
-// Output configuration loader
-// Loads and validates output configuration from YAML
-
-export function loadOutputConfig() {
-  // TODO: Implement config loading from config/output.yaml
-  return {};
-}
